e2d2e7714b171f088bf3e87ed0233d4c7cc2c454
Jaimie <jsjerome02@gmail.com>
2021-11-01 18:43:35 -0400

    correcting event listening code

--- static/js/app.js
+++ static/js/app.js
@@ -114,7 +114,7 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("#input").on("keyup", updateFilters)
+  d3.selectAll("#input").on("change", updateFilters)
   
   // Build the table when the page loads
   buildTable(tableData);
